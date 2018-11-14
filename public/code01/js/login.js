d65b41efd7a3e10d7afc5c8e40c82e14f18a5953
andersonyp <andersonyp@qq.com>
2018-11-14 15:15:52 +0800

    登录页面功能完成

--- public/code01/js/login.js
+++ public/code01/js/login.js
@@ -1,5 +1,6 @@
-// 功能1-实现字段校验
+
 $(function () {
+    // 功能1-实现字段校验
     /*
     *  校验规则
     *  1. 用户名不能为空
@@ -7,7 +8,6 @@ $(function () {
     *  3. 用户密码长度为6-12位
     *  4. 用户min长度为2-6位
     * */
-
     $('#form').bootstrapValidator({
         // 1-设置校验规则
         fields: {
@@ -22,6 +22,9 @@ $(function () {
                        min: 2,
                        max: 6,
                        message: '用户名长度必须是2-6位'
+                   },
+                   callback: {
+                       message: '用户名不存在'
                    }
                }
            },
@@ -34,9 +37,36 @@ $(function () {
                        min: 6,
                        max: 12,
                        message: '密码长度必须为6-12位'
+                   },
+                   callback: {
+                       message: '密码错误'
                    }
                }
             }
         }
+    })
+
+    // 功能2-注册表单校验成功事件
+    $('#form').on("success.form.bv",function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: '/employee/employeeLogin',
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function (info) {
+                if (info.success) {
+                    location.href = 'index.html';
+                }
+
+                if (info.error === 1000) {
+                    $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback');
+                }
+                
+                if (info.error === 1001) {
+                    $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback');
+                }
+            }
+        })
     })
 })
